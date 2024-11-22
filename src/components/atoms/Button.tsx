9fff5288cd79296bcf24f048f8767ecff1e79e38
Bernard Teye <datssosh@gmail.com>
2024-11-22 16:54:55 +0000

    refactor Button componet and complete ui for NotFound component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,6 +1,26 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef} from "react";
+import { Link } from "react-router-dom";
 
-export default function Button(props: ComponentPropsWithoutRef<"button">) {
+type ButtonProps = {
+  el: "button";
+} & ComponentPropsWithoutRef<"button">;
+
+type AnchorProps = {
+  el: "link";
+  relative?: "path" | "route";
+} & ComponentPropsWithoutRef<"a">;
+
+export default function Button(props: ButtonProps | AnchorProps) {
+  if (props.el === "link") {
+    const relative = props.relative ?? "route";
+
+    return (
+      <Link 
+        to={props.href!} {...props}
+        relative={relative}
+        className="bg-[var(--blue-100)] block text-white py-2 rounded-lg hover:bg-[var(--blue-200)] text-center">{props.children}</Link>)
+  }
+
   return (
     <button className="bg-[var(--blue-100)] block text-white py-2 rounded-lg hover:bg-[var(--blue-200)]">{props.children}</button>
   );
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,17 @@
+import Button from "../components/atoms/Button";
+import notFound from "../assets/img/404.png";
+
 export default function NotFound() {
   return (
-    <h1>Testing</h1>
+    <main 
+    className="w-full h-screen flex items-center justify-center bg-[url('/src/assets/img/auth-bg.png')] bg-cover bg-[var(--blue-100)]">
+      <section className="w-[90%] bg-white rounded-xl md:rounded-[24px] p-6 md:basis-2/3 md:p-[55px] lg:py-8 xl:py-16 xl:px-[50px] lg:basis-[500px]">
+        <img src={notFound} alt="404 error image" />
+        <h1 className="text-2xl md:text-[2em] mt-10 md:mt-16 mb-4 md:mb-6 lg:mb-4 xl:mb-6 text-center">Looks like you're lost...</h1>
+        <div className="flex flex-col">
+          <Button el="link" relative="path" href="..">Go Back Home</Button>
+        </div>
+      </section>
+    </main>
   );
 }
